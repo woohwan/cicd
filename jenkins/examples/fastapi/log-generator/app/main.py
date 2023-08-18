d5fe7411e7ac9c81b198c87b558e502fc68a8e5e
woohwan <whaprk70@naver.com>
2023-08-18 21:49:33 +0900

    change logger format

--- jenkins/examples/fastapi/log-generator/app/main.py
+++ jenkins/examples/fastapi/log-generator/app/main.py
@@ -1,13 +1,23 @@
 from fastapi import FastAPI, HTTPException
-import time
+import random
+import logging
 
-# https통신을 위해 수정
-app = FastAPI(title='EKS Project',
-              description='EFK Test',
-              openapi_url='/api/openapi.json')
+app = FastAPI()
 
 log_filename = "tenant.log"
-output = "output.log"
+logger = logging.getLogger(__name__)
+
+console_handler = logging.StreamHandler()
+# file_handler = logging.FileHandler('./output.log')
+
+formatter = logging.Formatter('%(message)s')
+console_handler.setFormatter(formatter)
+# file_handler.setFormatter(formatter)
+
+
+logger.addHandler(console_handler)
+# logger.addHandler(file_handler)
+logger.setLevel(level=logging.INFO)
 
 
 @app.get("/")
@@ -16,17 +26,15 @@ async def log():
 
 
 @app.get("/generate_raon_log/")
-async def generate_log(interval: int):
+async def generate_log():
 
-    result = "success"
+    result = ""
 
     with open(log_filename, "r") as logfile:
-        with open(output, 'w+') as outlog:
-            lines = logfile.readlines()
-            for line in lines:
-                print(line)
-                outlog.write(line)
-                outlog.flush()
-                time.sleep(interval)
+        lines = logfile.readlines()
+        size = len(lines)
+        line_number = random.randrange(0, size)
+        logger.info(lines[line_number])
+        result = lines[line_number]
 
     return result
